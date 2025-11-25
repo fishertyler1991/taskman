8f54d13b86e9d3eaadac1e497e08ae6cbaaec2c6
TFISH <fishertyler1991@gmail.com>
2025-11-25 08:01:11 -0700

    Reformatted as a class.

--- taskman.py
+++ taskman.py
@@ -2,39 +2,38 @@ import sys
 import palaToolkit as ptk
 import PyQt6.QtWidgets as _Q
 
-#Define global vars
 DEF_X = 100
 DEF_Y = 100
 DEF_W = 800
 DEF_H = 600
 
-#Define global classes/funcs
-
 def getProcs() -> str:
     return "Llol"
 
-#Define secondary global vars
+class TASKMANAPP(_Q.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PalaTaskMan")
+        self.setGeometry(DEF_X, DEF_Y, DEF_W, DEF_H)
 
-#Create main app and Window
+        overLabel = _Q.QWidget()
+        self.setCentralWidget(overLabel)
+        mainLayout = _Q.QVBoxLayout()
+        overLabel.setLayout(mainLayout)
+
+        self.textLabelA = _Q.QLabel("Task Manager")
+        self.buttonLabelA = _Q.QPushButton("Click this")
+        mainLayout.addWidget(self.textLabelA)
+        mainLayout.addWidget(self.buttonLabelA)
+
+        self.buttonLabelA.clicked.connect(self.BLA_Clicked)
+
+    def BLA_Clicked(self):
+        self.textLabelA.setText("Button Clicked!")
+
+
 mainApp = _Q.QApplication(sys.argv)
-mainWindow = _Q.QMainWindow()
-mainWindow.setWindowTitle("PalaTaskMan")
-mainWindow.setGeometry(DEF_X, DEF_Y, DEF_W, DEF_H)
-
-#Build base app
-overLabel = _Q.QWidget()
-mainWindow.setCentralWidget(overLabel)
-mainLayout = _Q.QVBoxLayout()
-textLabelA = _Q.QLabel("Task Manager")
-buttonLabelA = _Q.QPushButton("Click this")
-mainLayout.addWidget(textLabelA)
-mainLayout.addWidget(buttonLabelA)
-overLabel.setLayout(mainLayout)
-
-#Display app
+mainWindow = TASKMANAPP()
 mainWindow.show()
 
-#Start loop and handle exit
 sys.exit(mainApp.exec())
-
-#testing beta branch
